e88d729f3f193102bafe0845ad6212f936f27d2d
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2023-12-17 23:21:25 -0400

    Kumiko-MD

---
+++ plugins/info-ping.js
@@ -0,0 +1,50 @@
+import { totalmem, freemem } from 'os'
+import os from 'os'
+import util from 'util'
+import osu from 'node-os-utils'
+import { performance } from 'perf_hooks'
+import { sizeFormatter } from 'human-readable'
+import speed from 'performance-now'
+import { spawn, exec, execSync } from 'child_process'
+const format = sizeFormatter({ std: 'JEDEC', decimalPlaces: 2, keepTrailingZeroes: false, render: (literal, symbol) => `${literal} ${symbol}B` })
+
+var handler = async (m, { conn }) => {
+
+let timestamp = speed()
+let latensi = speed() - timestamp
+
+let _muptime = process.uptime() * 1000
+let muptime = clockString(_muptime)
+
+let chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
+let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
+
+
+let texto = `*▢ V E L O C I D A D*
+🚄 ${latensi.toFixed(4)}
+
+*▢ A C T I V I D A D*
+• ${muptime}
+
+*▢ C H A T S*
+• ${chats.length} *Chats privados*
+• ${groups.length} *Grupos*
+
+*▢ S E R V I D O R*
+🛜 *Ram:* ${format(totalmem() - freemem())} / ${format(totalmem())}`.trim()
+
+conn.sendMessage(m.chat, { text: texto, contextInfo: { externalAdReply: { title: '', body: 'CURIOSITY - BOT - MD', thumbnailUrl: 'https://telegra.ph/file/6cbf9148b572711e9b000.jpg', sourceUrl: '', mediaType: 1, renderLargerThumbnail: true }}})
+
+}
+handler.help = ['ping']
+handler.tags = ['info']
+handler.command = ['ping', 'speed']
+handler.register = true
+
+export default handler
+
+function clockString(ms) {
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
