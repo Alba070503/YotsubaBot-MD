cf6b8fc064ae7d71bd48b8f60b7ea8450183fa98
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-07-19 22:38:14 -0400

    🍁Rei Ayanami 🍁 By @Alba070503

---
+++ plugins/buscador-yts.js
@@ -0,0 +1,39 @@
+import yts from 'yt-search'
+
+var handler = async (m, { text, conn, args, command, usedPrefix }) => {
+
+if (!text) return conn.reply(m.chat, `⁖🧡꙰  *Escriba el título de algún vídeo de Youtube*\n\nEjemplo, !${command} Luffy Bot`, m, rcanal, )
+
+conn.reply(m.chat, wait, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: wm,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})
+
+let results = await yts(text)
+let tes = results.all
+let teks = results.all.map(v => {
+switch (v.type) {
+case 'video': return `⁖❤️꙰༻ *Título:* 
+» ${v.title}
+⁖🩵꙰༻ *Enlace:* 
+» ${v.url}
+⁖💜꙰༻ *Duración:*
+» ${v.timestamp}
+⁖💚꙰༻ *Subido:* 
+» ${v.ago}
+⁖🧡꙰༻ *Vistas:* 
+» ${v.views}`}}).filter(v => v).join('\n\n••••••••••••••••••••••••••••••••••••\n\n')
+
+conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, fkontak, m)
+
+}
+handler.help = ['ytsearch']
+handler.tags = ['buscador']
+handler.command = /^playlist|ytbuscar|yts(earch)?$/i
+
+handler.register = true
+handler.limit = true
+
+export default handler
