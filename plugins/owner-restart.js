d9293f546516387e621f925b65f742c45fb9191f
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-07-19 22:47:26 -0400

    🍁 Rei Ayanami 🍁 By @Alba070503

---
+++ plugins/owner-restart.js
@@ -0,0 +1,18 @@
+import { spawn } from 'child_process'
+
+var handler = async (m, { conn, isROwner, text }) => {
+
+if (!process.send) throw 'Dont: node rezero.js\nDo: node index.js'
+if (conn.user.jid == conn.user.jid) {
+await conn.reply(m.chat, '⁖💚꙰  *R E I N I C I A N D O* ⁖💚꙰', m, rcanal, )
+process.send('reset')
+} else throw 'eh'
+
+}
+handler.help = ['restart']
+handler.tags = ['owner']
+handler.command = ['restart','reiniciar'] 
+
+handler.rowner = true
+
+export default handler
