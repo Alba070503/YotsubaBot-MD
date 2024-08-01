a46e1a86cca9d483027f5cee1927b497dc9e398a
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-08-01 16:21:15 -0400

    Update info-host.js

--- plugins/info-host.js
+++ plugins/info-host.js
@@ -1,32 +1,4 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
-let cafirexostxt = `
-_Optimice la implementación de *GataBot* mediante la integración en un servicio de alojamiento de alto rendimiento._
-
-*🐈 Compatible con GataBot*
-Aprovecha la compatibilidad y comienza usar GataBot en servidores de alto rendimiento. El Staff de GataBot y Cafirexos hacen posible que puedas ejecutar las funciones que tanto te gusta usar sintiendo una experiencia fluida y de calidad.
-
-🔵 \`\`\`Información del Host\`\`\`
-
-💻 *Página*
-https://www.cafirexos.com
-
-✨ *Dashboard*
-https://dashboard.cafirexos.com
-
-⚙️ *Panel*
-https://panel.cafirexos.com
-
-📢 *Canal de WhatsApp*
-https://whatsapp.com/channel/0029VaFVSkRCMY0KFmCMDX2q
-
-💥 *Grupo de WhatsApp*
-https://chat.whatsapp.com/FBtyc8Q5w2iJXVl5zGJdFJ
-
-📧 *Correo*
-contacto@cafirexos.com
-
-🧑‍💻 *Contacto (Diego Flores)*
-https://wa.me/50497150165`
 let txt = `*¿Tu Nokia es muy lento y necesitas que tu bot esté activo 24/7?* 📱⏳
 
 ¡Tenemos la solución perfecta para ti! 🎉 Mantén tu bot funcionando sin interrupciones con nuestros servidores, Ofrecemos servidores gratuitos y de pago a precios súper accesibles, al alcance de todos. 💸 
@@ -51,7 +23,7 @@ https://chat.whatsapp.com/GQ82mPnSYnm0XL2hLPk7FV
 https://discord.com/invite/vgfpe4Nwd8
 
 🧡 *Canal de WhatsApp:*
-${canal1}
+https://whatsapp.com/channel/0029Va4QjH7DeON0ePwzjS1A
 
 🗣📲 *Contacto:*
 • https://www.facebook.com/elrebelde21
@@ -59,22 +31,6 @@ ${canal1}
 
 No esperes más y lleva tu bot al siguiente nivel con nuestro servicio de alojamiento. ¡Es fácil, rápido y económico! 💪🚀` 
 
-if (command == 'cafirexos') {
-await conn.sendFile(m.chat, 'https://grxcwmcwbxwj.objectstorage.sa-saopaulo-1.oci.customer-oci.com/n/grxcwmcwbxwj/b/cafirexos/o/logos%2Flogo.png', 'fantasy.jpg', cafirexostxt.trim(), fkontak, true, {
-contextInfo: {
-'forwardingScore': 200,
-'isForwarded': false,
-externalAdReply: {
-showAdAttribution: true,
-renderLargerThumbnail: false,
-title: `🔵 C A F I R E X O S 🔵`,
-body: `✅ Hosting de Calidad`,
-mediaType: 1,
-sourceUrl: accountsgb,
-thumbnailUrl: 'https://grxcwmcwbxwj.objectstorage.sa-saopaulo-1.oci.customer-oci.com/n/grxcwmcwbxwj/b/cafirexos/o/logos%2Flogo_2.png'
-}}
-}, { mentions: m.sender })
-}
 
 if (command == 'infinity' || command == 'infinityWa' || command == 'infohost' || command == 'hosting') {
  await conn.sendMessage(m.chat, { text: txt,
