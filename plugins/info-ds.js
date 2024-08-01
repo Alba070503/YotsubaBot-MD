c6cd98f7bacb0f6e6b6664ba7bd5b236b1b16ba5
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2024-08-01 18:00:50 -0500

    Update info-ds.js

--- plugins/info-ds.js
+++ plugins/info-ds.js
@@ -10,7 +10,7 @@ return conn.reply(m.chat, '💌 *Utiliza este comando directamente en el número
 }
 
 let chatId = m.isGroup ? [m.chat, m.sender] : [m.sender]
-let sessionPath = './AyanamiSession/'
+let sessionPath = './sessions/'
 
 try {
 
