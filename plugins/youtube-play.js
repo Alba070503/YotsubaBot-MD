eb4998afb16d6219209f8c18e10c57aba46467aa
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-08-08 08:23:32 -0400

    Update youtube-play.js

--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -6,90 +6,97 @@ let handler = async (m, { conn, args, usedPrefix, text, command }) => {
     if (!text) return conn.reply(m.chat, `*🚩 Ingresa el título de un video o música de YouTube.*`, m)
 
     try {
-        await m.react('🕓')
-        let res = await yts(text)
-        let vid = res.videos[0]
-        
-        if (!vid) return conn.reply(m.chat, `*☓ No se encontraron resultados para tu búsqueda.*`, m)
+        if (command === "play" || command === "play2") {
+            await m.react('🕓')
+            let res = await yts(text)
+            let vid = res.videos[0]
 
-        const infoTexto = `乂  Y O U T U B E   M U S I C\n
-        ✩ *Título ∙* ${vid.title}\n
-        ✩ *Duración ∙* ${vid.timestamp}\n
-        ✩ *Visitas ∙* ${vid.views}\n
-        ✩ *Autor ∙* ${vid.author.name}\n
-        ✩ *Publicado ∙* ${vid.ago}\n
-        ✩ *Url ∙* ${'https://youtu.be/' + vid.videoId}\n`.trim()
+            if (!vid) return conn.reply(m.chat, `*☓ No se encontraron resultados para tu búsqueda.*`, m)
 
-        await conn.sendButton(m.chat, infoTexto, wm, vid.thumbnail, [
-            ['Audio 📀', `${usedPrefix}mp3 ${text}`],
-            ['Video 🎥', `${usedPrefix}mp4 ${text}`],
-            ['AudioDoc 📀', `${usedPrefix}mp3doc ${text}`],
-            ['VideoDoc 🎥', `${usedPrefix}mp4doc ${text}`]
-        ], null, [['Canal', `https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04`]], m)
+            const infoTexto = `乂  Y O U T U B E   M U S I C\n
+            ✩ *Título ∙* ${vid.title}\n
+            ✩ *Duración ∙* ${vid.timestamp}\n
+            ✩ *Visitas ∙* ${vid.views}\n
+            ✩ *Autor ∙* ${vid.author.name}\n
+            ✩ *Publicado ∙* ${vid.ago}\n
+            ✩ *Url ∙* ${'https://youtu.be/' + vid.videoId}\n`.trim()
 
-        let q = command.includes('mp4') ? '360p' : '128kbps'
-        let dl_url, size, title
-        
-        if (command === 'mp3') {
-            let yt = await fg.yta(vid.url, q)
-            dl_url = yt.dl_url
-            size = yt.size.split('MB')[0]
-            title = yt.title
-        } else if (command === 'mp4') {
-            let yt = await fg.ytv(vid.url, q)
-            dl_url = yt.dl_url
-            size = yt.size.split('MB')[0]
-            title = yt.title
-        }
+            await conn.sendButton(m.chat, infoTexto, wm, vid.thumbnail, [
+                ['Audio 📀', `${usedPrefix}mp3 ${text}`],
+                ['Video 🎥', `${usedPrefix}mp4 ${text}`],
+                ['AudioDoc 📀', `${usedPrefix}mp3doc ${text}`],
+                ['VideoDoc 🎥', `${usedPrefix}mp4doc ${text}`]
+            ], null, [['Canal', `https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04`]], m)
+        } else {
+            await m.react('🕓')
+            let res = await yts(text)
+            let vid = res.videos[0]
 
-        const limit = 100
-        if (size >= limit) {
-            return conn.reply(m.chat, `El archivo pesa más de ${limit} MB, se canceló la descarga.`, m).then(_ => m.react('✖️'))
-        }
+            if (!vid) return conn.reply(m.chat, `*☓ No se encontraron resultados para tu búsqueda.*`, m)
 
-        if (command === 'mp3') {
-            await conn.sendMessage(m.chat, { 
-                audio: { url: dl_url }, 
-                mimetype: "audio/mpeg", 
-                fileName: `${title}.mp3`, 
-                quoted: m, 
-                contextInfo: {
-                    'forwardingScore': 200,
-                    'isForwarded': true,
-                    externalAdReply:{
-                        showAdAttribution: false,
-                        title: `${title}`,
-                        body: `${vid.author.name}`,
-                        mediaType: 2, 
-                        sourceUrl: `${vid.url}`,
-                        thumbnail: await (await fetch(vid.thumbnail)).buffer()
+            let q = command.includes('mp4') ? '360p' : '128kbps'
+            let dl_url, size, title
+            
+            if (command === 'mp3') {
+                let yt = await fg.yta(vid.url, q)
+                dl_url = yt.dl_url
+                size = yt.size.split('MB')[0]
+                title = yt.title
+            } else if (command === 'mp4') {
+                let yt = await fg.ytv(vid.url, q)
+                dl_url = yt.dl_url
+                size = yt.size.split('MB')[0]
+                title = yt.title
+            }
+
+            const limit = 100
+            if (size >= limit) {
+                return conn.reply(m.chat, `El archivo pesa más de ${limit} MB, se canceló la descarga.`, m).then(_ => m.react('✖️'))
+            }
+
+            if (command === 'mp3') {
+                await conn.sendMessage(m.chat, { 
+                    audio: { url: dl_url }, 
+                    mimetype: "audio/mpeg", 
+                    fileName: `${title}.mp3`, 
+                    quoted: m, 
+                    contextInfo: {
+                        'forwardingScore': 200,
+                        'isForwarded': true,
+                        externalAdReply:{
+                            showAdAttribution: false,
+                            title: `${title}`,
+                            body: `${vid.author.name}`,
+                            mediaType: 2, 
+                            sourceUrl: `${vid.url}`,
+                            thumbnail: await (await fetch(vid.thumbnail)).buffer()
+                        }
                     }
-                }
-            }, { quoted: m })
-        } else if (command === 'mp4') {
-            await conn.sendMessage(m.chat, { 
-                video: { url: dl_url }, 
-                caption: `${title}\n⇆ㅤㅤ◁ㅤㅤ❚❚ㅤㅤ▷ㅤㅤ↻\n00:15 ━━━━●────── ${vid.timestamp}`, 
-                mimetype: 'video/mp4', 
-                fileName: `${title}.mp4`, 
-                quoted: m, 
-                contextInfo: {
-                    'forwardingScore': 200,
-                    'isForwarded': true,
-                    externalAdReply:{
-                        showAdAttribution: false,
-                        title: `${title}`,
-                        body: `${vid.author.name}`,
-                        mediaType: 2, 
-                        sourceUrl: `${vid.url}`,
-                        thumbnail: await (await fetch(vid.thumbnail)).buffer()
+                }, { quoted: m })
+            } else if (command === 'mp4') {
+                await conn.sendMessage(m.chat, { 
+                    video: { url: dl_url }, 
+                    caption: `${title}\n⇆ㅤㅤ◁ㅤㅤ❚❚ㅤㅤ▷ㅤㅤ↻\n00:15 ━━━━●────── ${vid.timestamp}`, 
+                    mimetype: 'video/mp4', 
+                    fileName: `${title}.mp4`, 
+                    quoted: m, 
+                    contextInfo: {
+                        'forwardingScore': 200,
+                        'isForwarded': true,
+                        externalAdReply:{
+                            showAdAttribution: false,
+                            title: `${title}`,
+                            body: `${vid.author.name}`,
+                            mediaType: 2, 
+                            sourceUrl: `${vid.url}`,
+                            thumbnail: await (await fetch(vid.thumbnail)).buffer()
+                        }
                     }
-                }
-            }, { quoted: m })
+                }, { quoted: m })
+            }
+
+            await m.react('✅')
         }
-
-        await m.react('✅')
-
     } catch (error) {
         console.error(error)
         await conn.reply(m.chat, `*☓ Ocurrió un error inesperado.*`, m).then(_ => m.react('✖️'))
