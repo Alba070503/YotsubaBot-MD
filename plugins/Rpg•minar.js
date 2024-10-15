89e2429742fefa7acb1aae7039ee93928f915781
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-14 23:46:00 -0300

    Update Rpg•minar.js

--- plugins/Rpg•minar.js
+++ plugins/Rpg•minar.js
@@ -20,7 +20,7 @@ let info = `⛏️ *Te has adentrando en lo profundo de las cuevas*
 🕋 *Carbón*: ${coal}
 🪨 *Piedra*: ${stone}`
 
-conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '😊 *Minando.. - YaemoriBot* 🚩', 'status@broadcast' )
+conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '😊 *Minando.. - YotsubaBot* 🚩', 'status@broadcast' )
 await m.react('⛏️')
 
 user.health -= 50
