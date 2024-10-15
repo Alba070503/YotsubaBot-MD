f45bf8db77a8980e117d9d47e79c0166a9ded790
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-14 23:31:11 -0300

    Update Main•staff.js

--- plugins/Main•staff.js
+++ plugins/Main•staff.js
@@ -2,43 +2,22 @@ let handler = async (m, { conn, command, usedPrefix }) => {
 let staff = `🌹 *EQUIPO DE AYUDANTES*
 🌱 *Bot:* ${botname}
 🍟 *Versión:* ${vs}
-🪴 *Libreria:* ${libreria + baileys}
+🌙 *Libreria:* ${libreria + baileys}
 
 👑 *Propietario:*
 
-• DevDiego
-🌱 *Rol:* Propietario
+• Alba070503
+🍁 *Rol:* Propietario
 🌴 *Número:* ${creador}
-🚩 *GitHub:* https://github.com/Dev-Diego
+🍬 *GitHub:* https://github.com/Alba070503
 
-🪴  *Colaboradores:*
+🌸  *Colaboradores:*
 
-• GataDios
-🌱 *Rol:* Developer
-🌴 *Número:* Wa.me/593968263524
-🚩 *GitHub:* https://github.com/GataNina-Li
-
-
-• David Chian 
-🌱 *Rol:* Developer
-🌴 *Número:* Wa.me/5351524614
-🚩 *GitHub:* https://github.com/David-Chian
-
-• elrebelde21
-🌱 *Rol:* Developer
-🌴 *Número:* Wa.me/573147616444
-🚩 *GitHub:* https://github.com/elrebelde21
-
-• AzamiJs
-🌱 *Rol:* Developer
-🌴 *Número:* Wa.me/5214434703586
-🚩 *GitHub:* https://github.com/AzamiJs
-
-• GabrielVz
-🌱 *Rol:* Developer
-🌴 *Número:* Wa.me/5493794297363
-🚩 *GitHub:* https://github.com/glytglobal`
-await conn.sendFile(m.chat, icons, 'yaemori.jpg', staff.trim(), fkontak, true, {
+• Ian
+🍁 *Rol:* Developer
+🌴 *Número:* Wa.me/5493876639332
+🍬 *GitHub:* https://github.com/ianalejandrook15x`
+await conn.sendFile(m.chat, icons, 'yotsuba.jpg', staff.trim(), fkontak, true, {
 contextInfo: {
 'forwardingScore': 200,
 'isForwarded': false,
