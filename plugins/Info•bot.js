73a00d7cb68ec54ee48851dd68df126f641bbaa1
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-14 23:28:44 -0300

    Update Info•bot.js

--- plugins/Info•bot.js
+++ plugins/Info•bot.js
@@ -7,17 +7,15 @@ if (chat.isBaneed) return
 if (/^bot$/i.test(m.text)) {
 conn.reply(m.chat, `🚩 ¡Hola! Soy Yotsuba Nakano, en que puedo ayudarte hoy?\n\n✰ Usa *!menu* para ver mis comandos.`, m, rcanal, )
 }
-/*if (/^corin|corín|corinplus|corínplus|corinplushost|corínplushost|plus$/i.test(m.text)) {
-conn.reply(m.chat, `🚀 CorinPlus Hosting ¡El plus que necesitas!\n🚩 *Dash:* https://dash.corinplus.com\n🌱 *Panel:* https://ctrl.corinplus.com`, m, rcanal, )
-}*/
-if (/^sexo$/i.test(m.text)) {
-conn.reply(m.chat, `*pervertido* 🫣`, m, rcanal, )
+
+if (/^alba$/i.test(m.text)) {
+conn.reply(m.chat, `*Hola Alba es mi creador si tienes un problema o dudas comunicate con el 👇\n\n> Wa.me/59169214837`, m, rcanal, )
 }
-if (/^tetas|teta$/i.test(m.text)) {
-conn.reply(m.chat, `*que caliente eres* 🥵`, m, rcanal, )
+if (/^ian|ianalejandrook15x$/i.test(m.text)) {
+conn.reply(m.chat, `> *ianalejandrook15 es el staff oficial de YotsubaBot-MD si necesitas contactarte con el o quieres que te ayudes en tus proyectos te dejo su numero*\n\n> Wa.me/5493876639332\n\n> Pσɯҽɾҽԃ Bყ ιαɳαʅҽʝαɳԃɾσσƙ15x`, m, rcanal, )
 }
-if (/^bug$/i.test(m.text)) {
-conn.reply(m.chat, `*tu mamá we* 😹`, m, rcanal, )
+if (/^problema$/i.test(m.text)) {
+conn.reply(m.chat, `*Si presenta problemas o errores comunica al creador del bot* ⚠`, m, rcanal, )
 }
 if (/^pene$/i.test(m.text)) {
 conn.reply(m.chat, `*comes* 😹`, m, rcanal, )
