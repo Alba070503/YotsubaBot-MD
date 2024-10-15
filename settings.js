755acbe5d5b821f67e3a4120f7bf3f30d6eec34e
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-10-15 06:22:59 -0400

    🍀YotsubaBot-MD Powered @Alba070503

---
+++ settings.js
@@ -0,0 +1,107 @@
+import { watchFile, unwatchFile } from 'fs' 
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import cheerio from 'cheerio'
+import fetch from 'node-fetch'
+import axios from 'axios'
+import moment from 'moment-timezone' 
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+//BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
+//Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
+global.botNumberCode = '' //Ejemplo: +59169082575 
+global.confirmCode = ''
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.owner = [
+   ['59169082575', '𝚌𝚛𝚎𝚊𝚍𝚘𝚛✐', true],
+   ['59169214837', '𝚘𝚗𝚠𝚎𝚛 ඩා', true],
+   ['573012482597'],
+   ['5493794297363'],
+   ['5351524614']
+]
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.mods = []
+global.prems = []
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+//cambiar a true si el bot detecta sus propios comandos.
+global.isBaileysFail = false
+global.libreria = 'Baileys'
+global.baileys = 'V 6.7.8'
+global.vs = '2.0.7'
+global.languaje = 'Español'
+global.nameqr = 'YotsubaBot-MD✿'
+global.namebot = 'YotsubaBot-MD✿ᩚ'
+global.sessions = 'MiniSession'
+global.jadi = 'MiniJadiBot'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.packname = 'YotsubaBot-MD✿'
+global.botname = 'YotsubaBot-MD✻'
+global.wm = 'YotsubaBot-MD❋'
+global.author = '𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝙱𝚢 @Alba070503'
+global.dev = '© 𝚙𝚘𝚠𝚎𝚛𝚎𝚍 𝙱𝚢 *ੈ✩‧₊˚ 『𝚂𝚙𝚊𝚌𝚎𝙽𝚒𝚐𝚑𝚝 𝚃𝚎𝚊𝚖』 *ੈ✩‧₊˚'
+global.textbot = 'Yostuba Nakano : Powered By @Alba070503'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.imagen1 = fs.readFileSync('./src/img/Menu.jpg')
+global.imagen2 = fs.readFileSync('./src/img/Menu2.jpg')
+global.imagen3 = fs.readFileSync('./src/img/Menu3.jpg')
+global.welcome = fs.readFileSync('./src/img/welcome.jpg')
+global.adios = fs.readFileSync('./src/img/adios.jpg')
+global.catalogo = fs.readFileSync('./src/img/catalogo.jpg')
+global.miniurl = fs.readFileSync('./src/img/miniurl.jpg')
+global.avatar = fs.readFileSync('./src/img/avatar_contact.jpg')
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.grupo = 'https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04' //
+global.grupo2 = 'https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04' //
+global.grupo3 = 'https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04' //team oficial
+global.grupo4 = 'https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04' //
+global.grupo5 = 'https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04' //
+global.channel = 'https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04'
+global.channel2 = 'https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04'
+global.md = 'https://github.com/Alba070503/YotsubaBot-MD' 
+global.yt = 'https://youtube.com/@albao7o503'
+global.tiktok = 'https://tiktok.com/@Alba070503'
+global.correo = 'Alba070503@gmail.com'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: '❀ sᥙ́⍴ᥱr ᥕһᥲ𝗍sᥲ⍴⍴ ᑲ᥆𝗍 ☄︎', orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.esti = { key: {participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "6289643739077-1613049930@g.us" } : {})},message: {"videoMessage": { "title": `YotsubaBot-MD✰`, "h": `Hmm`,'seconds': '99999', 'gifPlayback': 'true', 'caption': `❀ Super Bot WhatsApp︎`, 'jpegThumbnail': catalogo }}}
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.cheerio = cheerio
+global.fs = fs
+global.fetch = fetch
+global.axios = axios
+global.moment = moment        
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.multiplier = 69
+global.maxwarn = '3'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'settings.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
