68180b5f40e47ab016d42fcd6d861a8851ffaf88
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-14 23:40:24 -0300

    Update Owner•leavegc.js

--- plugins/Owner•leavegc.js
+++ plugins/Owner•leavegc.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
 let chat = global.db.data.chats[m.chat]
 chat.welcome = false
-await conn.reply(id, `《✧》 *Ai Yaemori* Abandona El Grupo, Fué Genial Estar Aquí`) 
+await conn.reply(id, `《✧》 *Yotsuha* Abandona El Grupo, Fué Genial Estar Aquí`) 
 await conn.groupLeave(id)
 try {  
 chat.welcome = true
