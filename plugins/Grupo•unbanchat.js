74a40e5dc5d4dacd1f0eb1cc6050ff2b3a1cd99e
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-14 23:47:24 -0300

    Update Grupo•unbanchat.js

--- plugins/Grupo•unbanchat.js
+++ plugins/Grupo•unbanchat.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
 if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '🍭 *¡Este chat no está registrado!*', m, fake)
 let chat = global.db.data.chats[m.chat]
-if (!chat.isBanned) return conn.reply(m.chat, '🍟 *¡Ai Yaemori no está baneada en este chat!*', m, fake)
+if (!chat.isBanned) return conn.reply(m.chat, '🍟 *Yotsuba no está baneada en este chat!*', m, fake)
 chat.isBanned = false
-await conn.reply(m.chat, '🚩 *¡Ai Yotsuba ya fué desbaneada en este chat!*', m, fake)
+await conn.reply(m.chat, '🚩 *Yotsuba ya fué desbaneada en este chat!*', m, fake)
 }
 handler.help = ['unbanchat'];
 handler.tags = ['grupo'];
