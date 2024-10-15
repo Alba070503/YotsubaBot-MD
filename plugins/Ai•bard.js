e283673e97cb3ebaee370d837bc11150c7ab2a7f
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-14 23:19:30 -0300

    Update Ai•bard.js

--- plugins/Ai•bard.js
+++ plugins/Ai•bard.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 var handler = async (m, { text,  usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `🔎 *Ingresé una petición*\n\nEjemplo, ${usedPrefix + command} Conoces a Ai-Yaemori?`, m, rcanal)
+if (!text) return conn.reply(m.chat, `🔎 *Ingresé una petición*\n\nEjemplo, ${usedPrefix + command} Conoces a Yotsuba Nakano?`, m, rcanal)
 try {
 await m.react('🕒')
 var apii = await fetch(`https://aemt.me/bard?text=${text}`)
