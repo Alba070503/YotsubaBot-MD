f44650133008c6a3e1bb5aa0cccb06ac61658447
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-14 23:35:15 -0300

    Update Rpg•levelup.js

--- plugins/Rpg•levelup.js
+++ plugins/Rpg•levelup.js
@@ -2,15 +2,15 @@ import { canLevelUp, xpRange } from '../lib/levelling.js'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn }) => {
-let img = await (await fetch(`https://telegra.ph/file/b97148e2154508f63d909.jpg`)).buffer()
+let img = await (await fetch(`https://qu.ax/BltWe.jpg`)).buffer()
 let name = conn.getName(m.sender)
 let user = global.db.data.users[m.sender]
 if (!canLevelUp(user.level, user.exp, global.multiplier)) {
 let { min, xp, max } = xpRange(user.level, global.multiplier)
-let txt = `🍟 *Nombre* ${name}\n\n`
+let txt = `🍬 *Nombre* ${name}\n\n`
 txt += `🚩 *Nivel* ${user.level}\n`
 txt += `🍭 *XP* ${user.exp - min} / ${xp}\n\n`
-txt += `🐢 No es suficiente XP *${max - user.exp}* ¡De nuevo! ✨`
+txt += `🍁 No es suficiente XP *${max - user.exp}* ¡De nuevo! ✨`
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)}
 let before = user.level * 1
 while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
@@ -20,7 +20,7 @@ txt += `*${before}* ➔ *${user.level}* [ ${user.role} ]\n\n`
 txt += `• 🧬 Nivel anterior : ${before}\n`
 txt += `• 🧬 Nuevos niveles : ${user.level}\n`
 txt += `• 📅 Fecha : ${new Date().toLocaleString('id-ID')}\n\n`
-txt += `🚩 *Nota:* _Cuanto más a menudo interactúes con *Ai Yaemori*, mayor será tu nivel_`
+txt += `🚩 *Nota:* _Cuanto más a menudo interactúes con *Yotsuba Nakano*, mayor será tu nivel_`
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)}}
 
 handler.help = ['levelup']
