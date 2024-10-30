a95705a26e576ca41505e97809609a1ce2fce16d
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-30 10:33:17 -0500

    Actualizar Rg•verificar.js

--- plugins/Rg•verificar.js
+++ plugins/Rg•verificar.js
@@ -39,6 +39,17 @@ regbot += `•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•\n`
 regbot += `${packname}`
 await m.react('📩')
 await conn.sendMini(m.chat, '⊱『✅𝆺𝅥 𝗥𝗘𝗚𝗜𝗦𝗧𝗥𝗔𝗗𝗢(𝗔) 𝆹𝅥✅』⊰', textbot, regbot, imagen1, imagen1, channel, m)
+let chtxt = `👤 *Usuario*: ${m.pushName || 'Anónimo'}\n🌎 *Pais:* ${global.userNationality}\n🗃 *Verificación:* ${user.name}\n🌸 *Edad:* ${user.age} años\n🐢 *Bot:* ${packname}`.trim()
+await conn.sendMessage(global.channelid, { text: chtxt, contextInfo: {
+externalAdReply: {
+title: "🔔 Notificación General 🔔 ",
+body: '🐢 Un usuario registrado',
+thumbnailUrl: fotoperfil,
+sourceUrl: redes,
+mediaType: 1,
+showAdAttribution: false,
+renderLargerThumbnail: false
+}}}, { quoted: null })
 }
 handler.help = ['reg']
 handler.tags = ['rg']
