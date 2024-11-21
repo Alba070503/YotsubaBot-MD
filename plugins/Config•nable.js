86706e1fcdd409ee674fd707293760735e25d6f6
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-11-21 10:20:10 -0400

    Update Config•nable.js

--- plugins/Config•nable.js
+++ plugins/Config•nable.js
@@ -109,7 +109,7 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
           global.dfail('group', m, conn)
           throw false
         }
-      } else if (!isAdmin) {
+      } if (!(isAdmin || isOwner)) {
         global.dfail('admin', m, conn)
         throw false
       }
