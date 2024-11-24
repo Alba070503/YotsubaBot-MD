834dc2186a9b68b57e7484f2f174de8d4f4773a7
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-11-24 19:27:51 -0400

    Update Serbot•stop.js

--- plugins/Serbot•stop.js
+++ plugins/Serbot•stop.js
@@ -1,11 +1,20 @@
-let handler  = async (m, { conn }) => {
-if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, `🍟 El Bot principal no se puede apagar.`, m, fake)
-else {
-await conn.reply(m.chat, `🚩 Adiós Ai`, m, fake)
-conn.ws.close()
-}}
-handler.help = ['detener']
-handler.tags = ['serbot']
-handler.command = ['stop', 'apagar', 'detener', 'apagate', 'detenerai', 'aioff']
+import { jidNormalizedUser } from "@whiskeysockets/baileys";
+import Jadibots from "../lib/jadibots.js";
+let handler = async (m, { conn, text, isOwner }) => {
+    const parent = await Jadibots.conn;
+    let jid = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : conn.user?.jid || jidNormalizedUser(conn.user?.id);
+    if (!jid || jid === "@s.whatsapp.net") throw m.reply('Numero no valido');
+    if (jid !== conn.user?.jid && !isOwner) throw m.reply('Solo puede ser usado por mi owner');
+    const number = jid.split('@')[0];
+    if (!number) throw m.reply('Numero no valido');
+    if (!Jadibots.conns.has(number)) throw m.reply('Bot no activo');
+    await conn.reply(m.chat, "Goodbye bot!", m);
+    await Jadibots.conns.get(number)?.end?.();
+    await Jadibots.conns.delete(number);
+};
 
-export default handler
+handler.help = ["stopjadibot"];
+handler.tags = ["jadibot"];
+handler.command = /^stop(jadi)?bot$/i;
+
+export default handler;
