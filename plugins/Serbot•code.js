f593a682b91731b43d17aa15284797731c513052
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-11-24 19:30:15 -0400

    Update Serbot•code.js

--- plugins/Serbot•code.js
+++ plugins/Serbot•code.js
@@ -1,4 +1,4 @@
-const { useMultiFileAuthState, DisconnectReason, fetchLatestBaileysVersion, MessageRetryMap, makeCacheableSignalKeyStore, jidNormalizedUser, PHONENUMBER_MCC } = await import('@whiskeysockets/baileys');
+/*const { useMultiFileAuthState, DisconnectReason, fetchLatestBaileysVersion, MessageRetryMap, makeCacheableSignalKeyStore, jidNormalizedUser, PHONENUMBER_MCC } = await import('@whiskeysockets/baileys');
 import moment from 'moment-timezone';
 import NodeCache from 'node-cache';
 import readline from 'readline';
