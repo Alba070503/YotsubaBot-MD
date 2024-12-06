d412ac2993fb4996157fbea94278cf07dc79880c
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-12-06 18:17:25 -0400

    Update Sticker•sticker.js

--- plugins/Sticker•sticker.js
+++ plugins/Sticker•sticker.js
@@ -6,7 +6,7 @@ import { webp2png } from '../lib/webp2mp4.js'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
 let text1 = `✡︎ → Solicitado por:\n✡︎ → Bot:\n✡︎ → Made By:`
-let text2 = `✧ ${nombre}\n✧ YaemoriBot\n✧ Alba070503`
+let text2 = `✧ ${nombre}\n✧ YotsubaBot\n✧ Alba070503`
 let stiker = false
 try {
 let q = m.quoted ? m.quoted : m
