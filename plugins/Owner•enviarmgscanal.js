43635c0999f74de6c330348d0d1fe3f5856aa4bc
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-12-13 21:39:12 -0400

    Update Owner•enviarmgscanal.js

--- plugins/Owner•enviarmgscanal.js
+++ plugins/Owner•enviarmgscanal.js
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 let suggestionQueue = {};
-const idgroup = "120363351999685409@g.us";
+const idgroup = "120363347714830215@g.us";
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     let who = m.mentionedJid && m.mentionedJid.length > 0 ? m.mentionedJid[0] : (m.fromMe ? conn.user.jid : m.sender);
