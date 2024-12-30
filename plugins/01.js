82b1a53d94db76a6c965d92252a7ba471e2ee1ef
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-12-30 14:26:25 -0500

    Actualizar 01.js

--- plugins/01.js
+++ plugins/01.js
@@ -1,6 +1,8 @@
+import fetch from 'node-fetch'
+
 const handler = async (m, { conn }) => {
   const taguser = '@' + m.sender.split('@')[0]; // Obtiene el usuario etiquetado
-  
+
   conn.sendMessage(m.chat, {
     image: { url: 'https://qu.ax/MFOVJ.jpg' },
     caption: `You like me? ${taguser}`, // Mención visible del usuario
@@ -24,6 +26,7 @@ const handler = async (m, { conn }) => {
     viewOnce: true,
     headerType: 4,
     mentions: [m.sender], // Mención funcional
+    mentionedJid: await conn.parseMention(`You like me? ${taguser}`)
   }, { quoted: m });
 };
 
