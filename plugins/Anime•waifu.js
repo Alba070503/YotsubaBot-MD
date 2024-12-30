6867f9c97da959f86bef72b6500dcc28b201d024
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-12-30 14:46:24 -0500

    Actualizar Anime•waifu.js

--- plugins/Anime•waifu.js
+++ plugins/Anime•waifu.js
@@ -1,23 +1,61 @@
 import fetch from 'node-fetch'
 
-let handler = async (m, { conn, usedPrefix, command }) => {
-try {
-await m.react(emojis)
-conn.reply(m.chat, '🍀 Buscando Su *Waifu*', m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: dev,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})
-let res = await fetch('https://api.waifu.pics/sfw/waifu')
-if (!res.ok) return
-let json = await res.json()
-if (!json.url) return 
-await conn.sendFile(m.chat, json.url, 'thumbnail.jpg', '❒ *W A I F U* ❒', fkontak, null, rcanal)
-} catch {
-}}
+let handler = async (m, { conn }) => {
+    try {
+        await m.react(emojis)
+        conn.reply(m.chat, 'Ꙭ Buscando Su *Waifu*', m, {
+            contextInfo: {
+                externalAdReply: {
+                    mediaUrl: null,
+                    mediaType: 1,
+                    showAdAttribution: true,
+                    title: packname,
+                    body: wm,
+                    previewType: 0,
+                    thumbnail: icons,
+                    sourceUrl: channel
+                }
+            }
+        })
+
+        let res = await fetch('https://api.waifu.pics/sfw/waifu')
+        if (!res.ok) return
+        let json = await res.json()
+        if (!json.url) return
+
+   conn.sendMessage(m.chat, {
+    image: { url: json.url },
+    caption: `Miguel es tu patrona?`, 
+    footer: "Sock",
+    buttons: [
+      {
+        buttonId: ".mamar @5218711426787",
+        buttonText: {
+          displayText: "Yes",
+        },
+        type: 1,
+      },
+      {
+        buttonId: ".waifu",
+        buttonText: {
+          displayText: "No",
+        },
+        type: 1,
+      },
+    ],
+    viewOnce: true,
+    headerType: 4,
+  }, { quoted: m });
+
+    } catch (e) {
+        console.error(e)
+        conn.reply(m.chat, 'Hubo un error al intentar enviar el mensaje.', m)
+    }
+}
+
 handler.help = ['waifu']
 handler.tags = ['anime']
 handler.command = ['waifu']
-handler.register = true 
+handler.group = true
+handler.register = false
 export default handler
