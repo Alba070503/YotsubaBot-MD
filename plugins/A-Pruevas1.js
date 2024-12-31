16eeb85c793fa4edc27b0c01f7dd033f84ebcb7c
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-12-30 20:34:21 -0500

    Actualizar A-Pruevas1.js

--- plugins/A-Pruevas1.js
+++ plugins/A-Pruevas1.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
   m.reply('Hola bebé ✨️❤️')
   } catch (e) {
     console.error('Error al enviar hola', e);
-  }
+  }}
 
 handler.command = ['hola', 'hello']
 export default handler;
