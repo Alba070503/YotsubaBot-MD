14cd8361c39709fbdf31a9d8f900dc66848eae22
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2025-01-08 21:08:26 -0400

    Update Descargas-playV2.js

--- plugins/Descargas-playV2.js
+++ plugins/Descargas-playV2.js
@@ -1,38 +1,59 @@
+import fetch from 'node-fetch';
+import axios from 'axios';
+import yts from 'yt-search';
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (command === 'play') {
+        if (!text) return conn.reply(m.chat, `「 ✰ 」INGRESA UN *ENLACE* O *TITULO* DEL *AUDIO* QUE DESEA DESCARGAR DE *YOUTUBE*\n\n*• EJEMPLO:*\n> ${usedPrefix + command} Ryllz - Nemesis`, m);
+
+        try {
+            const search = await yts(text);
+            if (m.text.includes('http://') || m.text.includes('https://') || m.text.includes('youtube.com') || m.text.includes('youtu.be')) {
+                return conn.reply(m.chat, `「 ✰ 」DESCARGA NO VALIDA.\n\n> SI DESEA REALIZAR UNA *DESCARGA* DE LA PLATAFORMA DE *YOUTUBE* UTILIZANDO UN *ENLACE* DE DESCARGA, DEBE UTILIZAR LOS SIGUIENTES COMANDOS A CONTINUACION DEPENDIENDO DE LO QUE QUIERA DESCARGAR\n\n❀ */YTMP3* = AUDIO\n❀ */YTMP4* = VIDEO`, m);
+            }
+
+            let bodyv1 = `「 ✰ 」 *RESULTADOS ENCONTRADOS:*\n> BUSQUEDA: ${text}\n\n❀ *TITULO:* > ${search.videos[0].title}\n\`\`\`----------\`\`\`\n❀ *VISTAS:* > ${search.videos[0].views}\n\`\`\`----------\`\`\`\n❀ *DURACION:* > ${search.videos[0].duration}\n\`\`\`----------\`\`\`\n❀ *SUBIDO:* > ${search.videos[0].ago}\n\`\`\`----------\`\`\`\n❀ *URL:* > ${search.videos[0].url}\n\`\`\`----------\`\`\`\n\`ENVIANDO SUS RESULTADOS...\``;
+
+            conn.sendMessage(m.chat, { image: { url: search.videos[0].thumbnail }, caption: bodyv1 }, { quoted: m });
+
+const api = await axios.get(`https://api-rin-tohsaka.vercel.app/download/ytmp3?url=${search.videos[0].url}`)
+const response = await api.data.data.download;
+            
+      await conn.sendMessage(m.chat, { video: { url: response }, mimetype: 'audio/mpeg' }, { quoted: m });
+        } catch (e) {
+            conn.reply(m.chat, `「 ✰ 」OCURRIO UN FALLO AL PROCESAR SU SOLICITUD`, m);
+        }
+    }
+
+    if (command === 'play2') {
+        if (!text) return conn.reply(m.chat, `「 ✰ 」INGRESA UN *TITULO* DEL *VIDEO* QUE DESEA DESCARGAR DE *YOUTUBE*\n\n*• EJEMPLO:*\n> ${usedPrefix + command} Ryllz - Nemesis`, m);
+
+        try {
+            const search = await yts(text);
+            if (m.text.includes('http://') || m.text.includes('https://') || m.text.includes('youtube.com') || m.text.includes('youtu.be')) {
+                return conn.reply(m.chat, `「 ✰ 」DESCARGA NO VALIDA.\n\n> SI DESEA REALIZAR UNA *DESCARGA* DE LA PLATAFORMA DE *YOUTUBE* UTILIZANDO UN *ENLACE* DE DESCARGA, DEBE UTILIZAR LOS SIGUIENTES COMANDOS A CONTINUACION DEPENDIENDO DE LO QUE QUIERA DESCARGAR\n\n❀ */YTMP3* = AUDIO\n❀ */YTMP4* = VIDEO`, m);
+            }
+
+            let bodyv2 = `「 ✰ 」 *RESULTADOS ENCONTRADOS:*\n> BUSQUEDA: ${text}\n\n❀ *TITULO:* > ${search.videos[0].title}\n\`\`\`----------\`\`\`\n❀ *VISTAS:* > ${search.videos[0].views}\n\`\`\`----------\`\`\`\n❀ *DURACION:* > ${search.videos[0].duration}\n\`\`\`----------\`\`\`\n❀ *SUBIDO:* > ${search.videos[0].ago}\n\`\`\`----------\`\`\`\n❀ *URL:* > ${search.videos[0].url}\n\`\`\`----------\`\`\`\n\`ENVIANDO SUS RESULTADOS...\``;
+            conn.sendMessage(m.chat, { image: { url: search.videos[0].thumbnail }, caption: bodyv2 }, { quoted: m });
+
+const api = await axios.get(`https://api-rin-tohsaka.vercel.app/download/ytmp4?url=${search.videos[0].url}`)
+const response = await api.data.data.download;
+            
+            await conn.sendMessage(m.chat, { video: { url: response }, mimetype: 'video/mp4' }, { quoted: m });
+        } catch (e) {
+            conn.reply(m.chat, `「 ✰ 」OCURRIO UN FALLO AL PROCESAR SU SOLICITUD`, m);
+        }
+    }
+};
+
+handler.command = ['play', 'play2'];
+
+export default handler;
+
 /*
-❀ Plugin creado por @Alba070503
+> NO BORRAR ESTOS CREDITOS:
+• Código Original: Rin Tohsaka - Bot (Case)
+• Adaptación: Masha (Ai Hoshino - MD / Team Ascend)
+• Mejora y Funcionalidad: GabrielVz
 */
-
-
-import fetch from 'node-fetch'
-import yts from 'yt-search'
-
-let handler = async (m, { conn, text, args }) => {
-if (!text) {
-return m.reply("❀ ingresa un texto de lo que quieres buscar")
-}
-    
-let ytres = await search(args.join(" "))
-let txt = `- *Título* : ${ytres[0].title}
-- *Duración* : ${ytres[0].timestamp}
-- *Publicado* : ${ytres[0].ago}
-- *Canal* : ${ytres[0].author.name || 'Desconocido'}
-- *Url* : ${'https://youtu.be/' + ytres[0].videoId}`
-await conn.sendFile(m.chat, ytres[0].image, 'thumbnail.jpg', txt, m, rcanal)
-    
-try {
-let api = await fetch(`https://api.giftedtech.my.id/api/download/dlmp4?apikey=gifted&url=${ytres[0].url}`)
-let json = await api.json()
-let { quality, title, download_url } = json.result
-await conn.sendMessage(m.chat, { video: { url: download_url }, caption: `${title}`, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: m })
-} catch (error) {
-console.error(error)
-}}
-
-handler.command = /^(play2)$/i
-
-export default handler
-
-async function search(query, options = {}) {
-  let search = await yts.search({ query, hl: "es", gl: "ES", ...options })
-  return search.videos
-}
