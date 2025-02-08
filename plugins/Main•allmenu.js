bba22c762a67d5ad4f73b02b5fa1f93d7633c848
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2025-02-07 21:14:52 -0400

    Update Main•allmenu.js

--- plugins/Main•allmenu.js
+++ plugins/Main•allmenu.js
@@ -171,8 +171,8 @@ const pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://qu.ax/
 
 await m.react('⭐️') 
 
-//await conn.sendMessage(m.chat, { video: { url: vid }, caption: text.trim(), contextInfo: { mentionedJid: [m.sender], isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363198641161536@newsletter', newsletterName: '© Alba070503 - Channel 🍁', serverMessageId: -1, }, forwardingScore: 999, externalAdReply: { title: 'YotsubaBot-MD', body: dev, thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false,
-//}, }, gifPlayback: true, gifAttribution: 0 }, { quoted: fkontak })
+await conn.sendMessage(m.chat, { video: { url: vid }, caption: text.trim(), //contextInfo: { mentionedJid: [m.sender], isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363198641161536@newsletter', newsletterName: '© Alba070503 - Channel 🍁', serverMessageId: -1, }, forwardingScore: 999, externalAdReply: { title: 'YotsubaBot-MD', body: dev, thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false,
+                               }, gifPlayback: true, gifAttribution: 0 }, { quoted: fkontak })
 
   } catch (e) {
     await m.react(error)
