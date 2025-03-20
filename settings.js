08a05cb8f8ec109c88779257774a4db6231e2a18
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2025-03-20 05:49:15 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,90 +11,79 @@ import moment from 'moment-timezone'
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botNumberCode = '' //Ejemplo: +59169082575 
-global.confirmCode = ''
+global.botNumber = '' //Ejemplo: 573218138672
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 global.owner = [
-   ['59169082575', '𝚌𝚛𝚎𝚊𝚍𝚘𝚛✐', true],
-   ['59169214837', '𝚘𝚗𝚠𝚎𝚛 ඩා', true],
-   ['584245610338', '𝚘𝚗𝚠𝚎𝚛 🗿', true],
-   ['5218713339906'],
-   ['5804245610338'],
-   ['573012482597'],
-   ['5493794297363'],
-   ['595994841707']
-]
+  ['59169081575', '❀ Propietario ❀', true],
+  ['59169214837', '❀ Propietario ❀', true],
+  ['59169214837'],
+  ['595972157130'],
+  ['5212441357601'],
+  ['50557865603'],
+  ['5216671548329'],
+  ['584148256527'],
+  ['5212431268546'],
+  ['584128382768'],
+  ['5216646666666'],
+  ['5219666666666'],
+  ['593968263524']
+];
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 global.mods = []
+global.suittag = ['584120346669'] 
 global.prems = []
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-//cambiar a true si el bot detecta sus propios comandos.
-global.isBaileysFail = false
 global.libreria = 'Baileys'
-global.baileys = 'V 6.7.8'
-global.vs = '2.0.7'
+global.baileys = 'V 6.7.13' 
 global.languaje = 'Español'
-global.nameqr = 'YotsubaBot-MD✿'
-global.namebot = 'YotsubaBot-MD✿ᩚ'
-global.sessions = 'YotsubaSession'
-global.jadi = 'YotsubaJadiBot'
+global.vs = '2.2.0'
+global.nameqr = '❀YotsubaBot-MD❀'
+global.namebot = '✿ YotsubaBot-MD ✿'
+global.sessions = 'Sessions'
+global.jadi = 'JadiBots' 
+global.yukiJadibts = true
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.packname = 'YotsubaBot-MD✿'
-global.botname = 'YotsubaBot-MD✻'
-global.wm = 'YotsubaBot-MD❋'
-global.author = '𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝙱𝚢 @Alba070503'
-global.dev = '© 𝚙𝚘𝚠𝚎𝚛𝚎𝚍 𝙱𝚢 *ੈ✩‧₊˚ 『𝚂𝚙𝚊𝚌𝚎𝙽𝚒𝚐𝚑𝚝 𝚃𝚎𝚊𝚖』 *ੈ✩‧₊˚'
-global.textbot = 'Yostuba Nakano : Powered By @Alba070503'
+global.packname = '❀ YotsubaBot-MD ❀'
+global.botname = '❀ YotsubaBot-MD ❀'
+global.wm = '❀YotsubaBot-Sticker❀'
+global.author = '𝙲𝚛𝚎𝚊𝚝𝚎 @Alba070503'
+global.dev = '© PoWeReD @Alba070503'
+global.textbot = '𝚢𝙾𝚝𝚂𝚞𝙱𝚊𝙱𝚘𝚃-𝙼𝙳• Powered @Alba070503'
+global.etiqueta = 'ღ @Alba070503 ღ'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.imagen1 = fs.readFileSync('./src/img/Menu.jpg')
-global.imagen2 = fs.readFileSync('./src/img/Menu2.jpg')
-global.imagen3 = fs.readFileSync('./src/img/Menu3.jpg')
-global.welcome = fs.readFileSync('./src/img/welcome.jpg')
-global.adios = fs.readFileSync('./src/img/adios.jpg')
-global.catalogo = fs.readFileSync('./src/img/catalogo.jpg')
-global.miniurl = fs.readFileSync('./src/img/miniurl.jpg')
-global.avatar = fs.readFileSync('./src/img/avatar_contact.jpg')
+global.moneda = '¥enes'
+global.welcom1 = '❀ Edita Con El Comando setwelcome ❀'
+global.welcom2 = '❀ Edita Con El Comando setbye ❀'
+global.banner = 'https://files.catbox.moe/7yo1ig.jpg'
+global.avatar = 'https://files.catbox.moe/zy9ezo.jpg'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.grupo = 'https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04' //
-global.grupo2 = 'https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04' //
-global.grupo3 = 'https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04' //team oficial
-global.grupo4 = 'https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04' //
-global.grupo5 = 'https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04' //
+global.gp1 = 'https://chat.whatsapp.com/BqxPFOQFcOHFWDxXjk2BzQ'
+global.comunidad1 = 'https://chat.whatsapp.com/E5jwoN0vhhZGiBKh0B9bbX'
 global.channel = 'https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04'
 global.channel2 = 'https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04'
-global.md = 'https://github.com/Alba070503/YotsubaBot-MD' 
-global.yt = 'https://youtube.com/@albao7o503'
-global.tiktok = 'https://tiktok.com/@Alba070503'
-global.correo = 'Alba070503@gmail.com'
+global.md = 'https://github.com/Alba070503/YotsubaBot-MD'
+global.correo = 'Alba070503yt@gmail.com'
+global.cn ='https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04';
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
+global.catalogo = fs.readFileSync('./src/catalogo.jpg');
+global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 global.ch = {
 ch1: '120363198641161536@newsletter',
-ch2: '120363160031023229@newsletter',
-ch3: '120363263466636910@newsletter',
-ch4: '120363370415738881@newsletter',
 }
-
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
-global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: '❀ sᥙ́⍴ᥱr ᥕһᥲ𝗍sᥲ⍴⍴ ᑲ᥆𝗍 ☄︎', orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
-
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
-global.esti = { key: {participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "6289643739077-1613049930@g.us" } : {})},message: {"videoMessage": { "title": `YotsubaBot-MD✰`, "h": `Hmm`,'seconds': '99999', 'gifPlayback': 'true', 'caption': `❀ Super Bot WhatsApp︎`, 'jpegThumbnail': catalogo }}}
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
@@ -102,12 +91,62 @@ global.cheerio = cheerio
 global.fs = fs
 global.fetch = fetch
 global.axios = axios
-global.moment = moment        
+global.moment = moment   
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
-global.multiplier = 69
-global.maxwarn = '3'
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase();
+    const emot = {
+      level: '🌟 Nivel',
+      coin: '💸 Coin',
+      exp: '✨ Experiencia',
+      bank: '🏦 Banco',
+      diamond: '💎 Diamante',
+      health: '❤️ Salud',
+      kyubi: '🌀 Magia',
+      joincount: '💰 Token',
+      emerald: '♦️ Esmeralda',
+      stamina: '⚡ Energía',
+      role: '⚜️ Rango',
+      premium: '🎟️ Premium',
+      pointxp: '📧 Puntos Exp',
+      gold: '👑 Oro',
+      iron: '⛓️ Hierro',
+      coal: '🌑 Carbón',
+      stone: '🪨 Piedra',
+      potion: '🥤 Poción',
+    };
+    const results = Object.keys(emot).map((v) => [v, new RegExp(v, 'gi')]).filter((v) => v[1].test(string));
+    if (!results.length) return '';
+    else return emot[results[0][0]];
+  }};
+global.rpgg = { 
+  emoticon(string) {
+    string = string.toLowerCase();
+    const emott = {
+      level: '🌟',
+      coin: '💸',
+      exp: '✨',
+      bank: '🏦',
+      diamond: '💎',
+      health: '❤️',
+      kyubi: '🌀',
+      joincount: '💰',
+      emerald: '♦️',
+      stamina: '⚡',
+      role: '⚜️',
+      premium: '🎟️',
+      pointxp: '📧',
+      gold: '👑',
+      iron: '⛓️',
+      coal: '🌑',
+      stone: '🪨',
+      potion: '🥤',
+    };
+    const results = Object.keys(emott).map((v) => [v, new RegExp(v, 'gi')]).filter((v) => v[1].test(string));
+    if (!results.length) return '';
+    else return emott[results[0][0]];
+  }};  
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
